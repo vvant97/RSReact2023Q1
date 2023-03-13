7611155d13d2f0b96e944826c47aedcd9a0a44a7
Vladyslav Tyshchenko <vvant97@gmail.com>
2023-03-13 21:03:22 +0200

    feat: add content to Not Found page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,11 +1,22 @@
 import { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { FcQuestions as NotFoundIcon } from 'react-icons/fc';
+
+import './NotFound.scss';
 
 class NotFound extends Component {
   render() {
     return (
-      <main>
+      <main className="NotFound">
         <div className="container">
-          <h1>Not Found</h1>
+          <NotFoundIcon className="NotFoundIcon" />
+          <h1 className="NotFound-title">
+            The page you are looking for is not exist
+          </h1>
+
+          <Link className="NotFound-link" to="/">
+            Go to homepage
+          </Link>
         </div>
       </main>
     );
